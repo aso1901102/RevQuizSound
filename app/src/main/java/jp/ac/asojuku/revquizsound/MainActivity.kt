f4c53beeeeba33c61e2859ff62aba81f865af1fe
aso1901102 <1901102@s.asojuku.ac.jp>
2019-12-13 12:49:49 +0900

    ver.2.1

--- app/src/main/java/jp/ac/asojuku/revquizsound/MainActivity.kt
+++ app/src/main/java/jp/ac/asojuku/revquizsound/MainActivity.kt
@@ -1,12 +1,24 @@
 package jp.ac.asojuku.revquizsound
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        transforquestion.setOnClickListener {
+            //ボタンがクリックされた時の処理
+            //画面遷移用の院展とを生成
+            val intent = Intent(this,QuestActivity::class.java)
+            //intentをもとに画面遷移を実行
+            this.startActivity(intent)
+        }
     }
+
+
 }
--- app/src/main/java/jp/ac/asojuku/revquizsound/QuestActivity.kt
+++ app/src/main/java/jp/ac/asojuku/revquizsound/QuestActivity.kt
@@ -1,12 +1,24 @@
 package jp.ac.asojuku.revquizsound
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_quest.*
 
 class QuestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quest)
+
+            transforanswer.setOnClickListener {
+            //ボタンがクリックされた時の処理
+            //画面遷移用のインテントを生成
+            val intent = Intent(this,MainActivity::class.java)
+            //intentをもとに画面遷移を実行
+            this.startActivity(intent)
+        }
     }
+
+
 }
