7c0bbf10bf1f019b3ad73f4790fef64b159ee15f
aso1901102 <1901102@s.asojuku.ac.jp>
2019-12-15 23:19:00 +0900

    ver.3.0

--- app/src/main/java/jp/ac/asojuku/revquizsound/MainActivity.kt
+++ app/src/main/java/jp/ac/asojuku/revquizsound/MainActivity.kt
@@ -1,11 +1,20 @@
 package jp.ac.asojuku.revquizsound
 
 import android.content.Intent
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.SoundPool
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    //SoundPool型のインスタンス変数のフィールドプロパティを宣言
+    private lateinit var soundPool: SoundPool;
+    //効果音の音源(Sound)のリソースID
+    //仮の初期値で初期化
+    private  var answer = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,12 +22,39 @@ class MainActivity : AppCompatActivity() {
 
         transforquestion.setOnClickListener {
             //ボタンがクリックされた時の処理
-            //画面遷移用の院展とを生成
+            //画面遷移用のインテントを生成
             val intent = Intent(this,QuestActivity::class.java)
             //intentをもとに画面遷移を実行
             this.startActivity(intent)
         }
+
+        answerButton.setOnClickListener {
+            soundPool.play(answer,1.0f,1.0f,0,0,1.0f);
+        }
     }
+
+    override fun onResume() {
+        super.onResume()
+        //画面が表示されている間だけインスタンスをメモリに保持する
+        //SoundPoolクラスのインスタンスを変数に代入
+        this.soundPool =
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                //4.4 LOLLIPOPより古い端末(非推奨のメソッドを利用）
+                SoundPool(     //SoundPoolクラスのコンストラクタ
+                    2,                  //同時にならせる音源の数
+                    AudioManager.STREAM_ALARM,  //オーディオの種類
+                    0                 //音源の音質。
+                );
+            } else {
+                val audioAttributes = AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_ALARM).build();
+                //オーディオ設定を使ってSoundPoolのインスタンスを作る
+                SoundPool.Builder().setMaxStreams(1)                //同時音源数
+                    .setAudioAttributes(audioAttributes).build();   //オーディオ設定を登録してビルド
+            }
+        this.answer = soundPool.load(this,R.raw.answer,1)
+    }
+
 
 
 }
--- app/src/main/java/jp/ac/asojuku/revquizsound/QuestActivity.kt
+++ app/src/main/java/jp/ac/asojuku/revquizsound/QuestActivity.kt
@@ -1,24 +1,75 @@
 package jp.ac.asojuku.revquizsound
 
 import android.content.Intent
+import android.media.AudioAttributes
+import android.media.AudioManager
+import android.media.SoundPool
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_quest.*
 
 class QuestActivity : AppCompatActivity() {
+    //SoundPool型のインスタンス変数のフィールドプロパティを宣言
+    private lateinit var soundPool: SoundPool;
+    //効果音の音源(Sound)のリソースID
+    //仮の初期値で初期化
+    private  var uncorrect = 0;
+    private  var correct = 0;
+    private  var clap = 0;
+    private  var question = 0;
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quest)
-
-            transforanswer.setOnClickListener {
+        transforanswer.setOnClickListener {
             //ボタンがクリックされた時の処理
             //画面遷移用のインテントを生成
             val intent = Intent(this,MainActivity::class.java)
             //intentをもとに画面遷移を実行
             this.startActivity(intent)
         }
+
+        buttonFalse.setOnClickListener {
+            soundPool.play(uncorrect,1.0f,1.0f,0,0,1.0f);
+        }
+
+        buttonTrue.setOnClickListener {
+            soundPool.play(correct,1.0f,1.0f,0,0,1.0f);
+        }
+
+        buttonClap.setOnClickListener {
+            soundPool.play(clap,1.0f,1.0f,0,0,1.0f);
+        }
+
+        buttonQuestion.setOnClickListener {
+            soundPool.play(question,1.0f,1.0f,0,0,1.0f);
+        }
     }
 
-
+    override fun onResume() {
+        super.onResume()
+        //画面が表示されている間だけインスタンスをメモリに保持する
+        //SoundPoolクラスのインスタンスを変数に代入
+        this.soundPool =
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                //4.4 LOLLIPOPより古い端末(非推奨のメソッドを利用）
+                SoundPool(     //SoundPoolクラスのコンストラクタ
+                    2,                  //同時にならせる音源の数
+                    AudioManager.STREAM_ALARM,  //オーディオの種類
+                    0                 //音源の音質。
+                );
+            } else {
+                val audioAttributes = AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_ALARM).build();
+                //オーディオ設定を使ってSoundPoolのインスタンスを作る
+                SoundPool.Builder().setMaxStreams(1)                //同時音源数
+                    .setAudioAttributes(audioAttributes).build();   //オーディオ設定を登録してビルド
+            }
+        this.uncorrect = soundPool.load(this,R.raw.uncurrect,1)
+        this.correct = soundPool.load(this,R.raw.currect,1)
+        this.clap = soundPool.load(this,R.raw.clappinghands,1)
+        this.question = soundPool.load(this,R.raw.powerup10,1)
+    }
 }
